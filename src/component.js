efa75847e109890c9ec92e224f3637254d8011c4
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-08-03 18:59:10 -0400

    Cleaned up code
    Put the component parts of my code in component.js

--- src/component.js
+++ src/component.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { useSpring, animated } from "react-spring";
 
 export default function Component() {
+  const props = useSpring({ color: "green", from: { color: "blue" } });
+
   return (
-    <div>
-      <h1>Component 1</h1>
-      <p>Warm 30</p>
+    <div className="App" style={c1Style}>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happesn!</h2>
     </div>
   );
 }
+
+//So you can use objects to implement styles in components, interesting
+const c1Style = {
+  background: "steelblue",
+  color: "white",
+  padding: "1.5rem"
+};
--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { useSpring, animated } from "react-spring";
 import Component from "./component";
 
 import "./styles.css";
 
 function App() {
-  const props = useSpring({ color: "green", from: { color: "blue" } });
- 
   return (
-    <div className="App" style={c1Style}>
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happesn!</h2>
+    <div className="App">
+      <Component />
     </div>
   );
 }
 
 //So you can use objects to implement styles in components, interesting
 const c1Style = {
-  background:'steelblue',
-  color: 'white',
-  padding: '1.5rem'
-}
+  background: "steelblue",
+  color: "white",
+  padding: "1.5rem"
+};
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
