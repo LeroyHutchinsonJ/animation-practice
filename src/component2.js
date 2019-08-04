92c9e83ff4b1a10c3d7d77c8394521f3563cdb0f
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-08-04 12:17:42 -0400

    Styled up the components
    Changed the background color, and added a number tracker that goes from 1 to 10

--- src/component2.js
+++ src/component2.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { Children } from "react";
 import { useSpring, animated, config } from "react-spring";
+import { Spring } from "react-spring/renderprops";
 
-var AnimatedPart = () => {
+var AnimatedPart = words => {
   var props = useSpring({
-    from: { marginTop: -500 },
-    to: { marginTop: 0 },
-    config: { duration: 2000 },
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    config: { duration: 1000 },
     delay: 1000
   });
   return (
     <animated.div style={props}>
-      <div style={c2Style}>Check</div>
+      <div style={c2Style}>{words.children}</div>
     </animated.div>
   );
 };
 
 class Component2 extends React.Component {
   render() {
-    return <AnimatedPart />;
+    return (
+      <AnimatedPart>
+        <Spring
+          from={{ number: 1 }}
+          to={{ number: 10 }}
+          config={{ delay: 1000, duration: 10000 }}
+        >
+          {props => <div style={numberStyle}>{props.number.toFixed()}</div>}
+        </Spring>
+      </AnimatedPart>
+    );
   }
 }
 
@@ -25,5 +36,14 @@ const c2Style = {
   background: "red",
   color: "white",
   padding: "1.5rem"
+};
+const numberStyle = {
+  background: "#333",
+  color: "white",
+  padding: "1.5rem",
+  borderRadius: "100%",
+  width: "30px",
+  textAlign: "center",
+  margin: "auto"
 };
 export default Component2;
