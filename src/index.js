584c75e35ce60ad9ef8c4817dde7e6deaba2da96
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-08-11 11:17:44 -0400

    Hooky issues
    Trying to figure out why my animation will not activate when using it with item.show

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import Component1 from "./component1";
 import Component2 from "./component2";
 import Component3 from "./component3";
-import { useTransition, animated } from "react-spring";
+
+import { useTransition, animated, Transition } from "react-spring";
 
 import "./styles.css";
 
@@ -11,15 +13,18 @@ var AnimateComponent3 = show => {
   var transitions = useTransition(show, null, {
     from: { opacity: 0 },
     enter: { opacity: 1 },
-    leave: { opacity: 0 }
+    to: { opacity: 0 }
   });
 
-  return transitions.map(({ item, key, props }) => (
-    <animated.div key={key} style={props}>
-      <Component3 />
-      {console.log(item)}
-    </animated.div>
-  ));
+  return transitions.map(
+    ({ item, key, props }) =>
+      item.show && (
+        <animated.div key={key} style={props}>
+          <Component3 />
+          {console.log(item.show)}
+        </animated.div>
+      )
+  );
 };
 
 class App extends React.Component {
@@ -35,29 +40,15 @@ class App extends React.Component {
       <div className="App">
         <Component1 />
         <Component2 toggle={() => this.toggleFunction()} />
-        {this.state.showComponent3 && (
-          <AnimateComponent3 show={this.state.showComponent3} />
-        )}
+
+        <AnimateComponent3 show={this.state.showComponent3} />
       </div>
     );
   }
 }
 
 /*
-<Transition
-          native
-          items={this.state.showComponent3}
-          from={{ opacity: 0 }}
-          enter={{ opacity: 1 }}
-          leave={{ opacity: 0 }}
-        >
-          {showComponent3 => showComponent3 && (props => (
-              <animated.div style={props}>
-                <Component3 />
-               
-              </animated.div>
-            ))}
-        </Transition>
+
 */
 
 const rootElement = document.getElementById("root");
